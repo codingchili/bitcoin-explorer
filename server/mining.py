869185c0a20945305780c0f5e00ba885d1356a99
Robin Duda <rduda@kth.se>
2021-05-13 22:50:44 +0200

    btc mining; block hash, merkle root & nonces.

--- server/mining.py
+++ server/mining.py
@@ -1,76 +1,108 @@
 import asyncio
 
 from server.btcrpc import *
-from binascii import *
 from server.decode_block import *
+from binascii import *
+
+import io
 import time
 
-
-# BLOCK HEADER
-# 80 BYTES HEADER
-# TX DATA
+coinbase = b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+address = '76a914071e31b8289aa9d80b970230cb1b8b76466f2ec488ac'
 
 
-# HEADER
-# 4 BYTES VERSION
-# 32 BYTES LAST BLOCK HASH
-# 32 BYTES MERKLE ROOT FOR TX
-# 4 BYTES BLOCK TIEM
-# 4 BYTES FROM BLOCK TEMPLATE, "BITS"
-# 4 BYTES FOR NONCE
+def to_bytes(int, size=1):
+    return int.to_bytes(size, 'little')
 
-# THEN 2xSHA
 
-# TXS
-# INPUT
-# 32 BYTES FOR TX, 0X000 FOR CONIBASE
-# 4 BYTES OUTUT, 0XFF FOR COINBASE
-# 1-4 BYTES, SCRIPTLEN, SCRIPTSIG LEN
-# SOMETHING SOMETHING BIP34
-# SOME SCRIPT, NOT REQUIRED FOR COINBASE
-# SEQUENCE, FOR COINBASE: 0Xff ff ff ff
-
-def get_header(template):
+def get_header(template, merkle, nonce=1):
     version = b'\x00\x00\x00\x20'
 
-    previous = unhexlify(template['result']['previousblockhash'][::-1])
-    timestamp = int(time.time()).to_bytes(4, 'little')
-    bits = unhexlify(template['result']['bits'][::-1])
-    nonce = b'\x00\x00\x00\x04'[::-1]
-    merkle = b'\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF\xFF'
+    previous = unhexlify(template['previousblockhash'][::-1])
+    timestamp = to_bytes(int(time.time()), 4)
+    bits = unhexlify(template['bits'][::-1])
+    nonce = to_bytes(nonce, 4)
+    txcount = b'\x01'
 
-    header = (version + previous + merkle + timestamp + bits + nonce)
+    header = (version + previous + merkle + timestamp + bits + nonce + txcount)
     return header
 
-def get_transactions():
+
+def get_transaction(template):
     txver = b'\x01\x00\x00\x00'
-    txcount = b'\x01'
-    return txver + txcount + get_inputs() + get_outputs()
+    return txver + get_inputs(template) + get_outputs(template)
 
 
-def get_inputs():
+def get_inputs(template):
     count = b'\x01'
-    hash =  b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
+    hash = coinbase
     index = b'\xff\xff\xff\xff'
-    scriptLen = b'\x03'
-    script = b'\x02\xbc\x04'
+    height = to_bytes(template["height"], 3)
+    script_len = to_bytes(len(height) + 1)
+    script = to_bytes(len(height)) + height
     sequence = b'\xFF\xFF\xFF\xFF'
-    return count + hash + index + scriptLen + script + sequence
+    return count + hash + index + script_len + script + sequence
 
-def get_outputs():
+
+def get_outputs(template):
     count = b'\x01'
-    toshis = int(50 * 1_000_000).to_bytes(8, 'little')
-    pubScript = unhexlify('76a914071e31b8289aa9d80b970230cb1b8b76466f2ec488ac')
-    scriptLen = len(pubScript).to_bytes(1, 'little')
+    toshis = to_bytes(template['coinbasevalue'], 8)
+    pubScript = unhexlify(address)
+    scriptLen = to_bytes(len(pubScript))
     lock = b'\x00\x00\x00\x00'
     return count + toshis + scriptLen + pubScript + lock
 
+
+def b2str(rawbytes):
+    return binascii.hexlify(rawbytes).decode()
+
+
+def dsha256(b):
+    return hashlib.sha256(hashlib.sha256(b).digest()).digest()
+
+def hash50k(template):
+    block = None
+    best = None
+    transactions = get_transaction(template)
+    merkle = dsha256(transactions)
+
+    start = time.time()
+    for i in range(0, 850000):
+        header = get_header(template, merkle, i)
+        hash = b2str(dsha256(header))
+
+        if block is None:
+            block = header
+
+
+        if hash < best:
+            best = hash
+            block = header
+            print("new best hash: " + best)
+
+    print("best hash is: ")
+    print(best)
+
+    # submit block+transactions
+
+    end = time.time()
+    print(f"finish in {end - start}s")
+
 async def run():
     rpc = BTCRPC("userbob", "secret")
-    template = await rpc.blocktemplate()
+    template = (await rpc.blocktemplate())["result"]
 
-    header = get_header(template)
-    transactions = get_transactions()
+    transactions = get_transaction(template)
+    merkle = dsha256(transactions)
+    header = get_header(template, merkle)
+
+    hash50k(template)
+
+    block = header + transactions
+    raw = io.BytesIO(block)
+
+    print(b2str(dsha256(raw.read(80))[::-1]))
+    print(b2str(dsha256(transactions)[::-1]))
 
     decodeBlock(header + transactions)
 
@@ -83,13 +115,3 @@ try:
     loop.run_forever()
 except KeyboardInterrupt:
     print("sigterm by user")
-
-#{'result': {'capabilities': ['proposal'], 'version': 536870912, 'rules': ['csv', '!segwit'], 'vbavailable': {},
-#            'vbrequired': 0, 'previousblockhash': '000000000ea9dc6077225e90c31954ed2bdbfb3f5eeb433a14aa871ad47d959f',
-#            'transactions': [], 'coinbaseaux': {}, 'coinbasevalue': 5000000000,
-#            'longpollid': '000000000ea9dc6077225e90c31954ed2bdbfb3f5eeb433a14aa871ad47d959f144067',
-#            'target': '00000000e04b0000000000000000000000000000000000000000000000000000', 'mintime': 1620672761,
-#            'mutable': ['time', 'transactions', 'prevblock'], 'noncerange': '00000000ffffffff', 'sigoplimit': 80000,
-#            'sizelimit': 4000000, 'weightlimit': 4000000, 'curtime': 1620676057, 'bits': '1d00e04b', 'height': 144065,
-#            'default_witness_commitment': '6a24aa21a9ede2f61c3f71d1defd3fa999dfa36953755c690689799962b48bebd836974e8cf9'},
-# 'error': None, 'id': None}
